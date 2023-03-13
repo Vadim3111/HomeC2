808baaa6e2d6cccc696dcc402e7147c8acfea949
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-13 12:34:38 +0300

    Done Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <=5)
+    Console.WriteLine("Нет");
+else
+    Console.WriteLine("Да");
